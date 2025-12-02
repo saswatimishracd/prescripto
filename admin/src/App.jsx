c72666eed69491626a0b649018b304776a0bb72e
Sushree Saswati Mishra <saswatimishracd@gmail.com>
2025-12-02 21:48:16 +0530

    Project Completed

---
+++ admin/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login.jsx'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext.jsx';
+import Navbar from './components/Navbar.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import AllAppointments from './pages/Admin/AllAppointments.jsx';
+import Dashboard from './pages/Admin/Dashboard.jsx';
+import AddDoctor from './pages/Admin/AddDoctor.jsx';
+import DoctorsList from './pages/Admin/DoctorsList.jsx';
+import { DoctorContext } from './context/doctorContext.jsx';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments.jsx';
+import DoctorProfile from './pages/Doctor/DoctorProfile.jsx';
+
+const App = () => {
+
+  const {aToken} = useContext(AdminContext)
+  const {dToken} = useContext(DoctorContext)
+
+  return aToken || dToken ? (
+    <div className='bg-[#F8F9FD]'>
+      <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          {/* Admin Route */}
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+
+          {/* Doctor Route */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
